64f7d15c10a3e4ff7b56b77f72065f32fe2c8b03
wlarodri <wlarodri@hotmail.es>
2014-04-09 21:33:51 -0400

    Proyecto Nuevo

---
+++ src/Hola/Main.java
@@ -0,0 +1,15 @@
+package Hola;
+
+class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Los numeros impares son:");
+for(int i=0;i<100;i++){
+	if(i%2==1){
+		System.out.println("-" +i);
+	}
+
+}
+	}
+}
