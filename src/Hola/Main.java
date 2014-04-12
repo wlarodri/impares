2ada22f0a680e0b69cdea4592f970f120e6bcc29
wlarodri <wlarodri@hotmail.es>
2014-04-12 19:54:54 -0400

    Numeros impares del 1 al 100

--- src/Hola/Main.java
+++ src/Hola/Main.java
@@ -4,7 +4,7 @@ class Main {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Los numeros impares son:");
+		System.out.println("Los numeros impares son (1->99):");
 for(int i=0;i<100;i++){
 	if(i%2==1){
 		System.out.println("-" +i);
